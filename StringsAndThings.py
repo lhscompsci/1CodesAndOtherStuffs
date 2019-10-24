6732a589c4e3c631fa943ba1f9083ca921b39c3d
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-24 09:02:40 -0500

    searching

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -49,3 +49,13 @@ print(name[6:])
 
 for i in range(1, len(name)+1):
     print(name[0:i])
+
+# Searching inside of Strings
+
+print("biv" in name)
+print("v" not in name)
+
+if "y" in name:
+    print("The letter y is in name")
+else:
+    print("The letter y is not in name")
