df6fec548a6aa7a88a38e809539e765a81f3721a
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-29 08:43:13 -0500

    Added crypto.py and the transposition encryption function

---
+++ crypto.py
@@ -0,0 +1,18 @@
+# Transposition Cipher
+
+# original: this_is_a_secret_message_that_i_want_to_transmit
+# encrypted:hsi__ertmsaeta__att_rnmtti_sasce_esg_htiwn_otasi
+
+def scramble2Encrypt(plainText):
+    evenChars = ""
+    oddChars = ""
+    charCount = 0
+    for ch in plainText:
+        if charCount % 2 == 0:
+            evenChars = evenChars + ch
+        else:
+            oddChars = oddChars + ch
+        charCount = charCount + 1
+    cipherText = oddChars + evenChars
+    return cipherText
+
